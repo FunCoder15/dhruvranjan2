cfa71a9c8ae1bf0e604273fa8cffcc31975fbc30
Dhruv <97015100+FunCoder15@users.noreply.github.com>
2022-06-24 10:54:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var sidenav = document.getElementById('sidenav');
+function OpenSideNav() {
+    sidenav.style.width = '100vw';
+    sidenav.style.fontSize = '1.5em';
+}
+function CloseSideNav() {
+    sidenav.style.width = '0';
+    sidenav.style.fontSize = '0';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
